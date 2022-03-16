a965875ee13bc52633d313240a18e4d5e0f4f9ab
Carolina Torregrosa Nadal <carolatn@gmail.com>
2022-03-16 16:36:50 +0100

    [MOD] Ejercicio 3. Extracción de constantes desde cadenas y números del main

--- src/main/java/ed/carolina/Institut.java
+++ src/main/java/ed/carolina/Institut.java
@@ -2,14 +2,19 @@ package ed.carolina;
 
 public class Institut {
 
+	private static final int ANY = 2022;
+	private static final String CICLE_FORMATIU = "1º DAM";
+	private static final String COGNOMS_ALUMNE = "Torregrosa";
+	private static final String NOM_ALUMNE = "Carolina";
+
 	public static void main(String[] args) {
 		
 		Alumne p = new Alumne();
 		
-		p.nom = "Carolina";
-		p.cognoms = "Torregrosa";
-		p.cicleFormatiu = "1º DAM";
-		p.any = 2022;
+		p.nom = NOM_ALUMNE;
+		p.cognoms = COGNOMS_ALUMNE;
+		p.cicleFormatiu = CICLE_FORMATIU;
+		p.any = ANY;
 		
 		System.out.println(p.toString());
 
